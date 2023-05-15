9931898551ec6c4f7d309d93400bfe10c37de9aa
b00132407 <91259835+b00132407@users.noreply.github.com>
2023-05-15 12:21:57 +0100

    Update script

--- Scripts/addresses.py
+++ Scripts/addresses.py
@@ -6,6 +6,7 @@ url = 'https://api.ransomwhe.re/export'
 response = requests.get(url)
 data = response.json()
 
+#Write all data to file and filter based on Ransomware family name
 with open('RushSanctionOutput.txt', 'w') as file:
     for item in data['result']:
         if item['family'] == 'Rush/Sanction':
